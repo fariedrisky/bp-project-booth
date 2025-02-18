a22dedfcb9f8c12fda7afaf996722ee98f50d7cf
Faried Risky <fariedrisky@gmail.com>
2025-02-19 00:52:01 +0700

    feat: better SEO

--- src/data/images/logo.ts
+++ src/data/images/logo.ts
@@ -1,6 +1,6 @@
 import logo from "../../../public/assets/images/logo.svg";
 import logowhite from "../../../public/assets/images/logo-white.svg";
-import logo1x1 from "../../../public/assets/images/1x1.jpg";
+import logo1x1 from "../../../public/assets/images/og-home.jpg";
 
 export {
     logo,
